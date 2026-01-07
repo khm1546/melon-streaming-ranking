758a81fd0a3f3fbae25e40412157b35452627ab3
김형민_이모션 <hyeongmin.kim@emotion.co.kr>
2026-01-07 16:55:59 +0900

    문법 수정

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -34,7 +34,6 @@ const Hero = () => {
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
-        margin-top="60px"
       >
         <motion.h1
           className="hero-title"
