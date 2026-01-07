7e936eb0d111885a5a96ba327c4699c8923a6c62
김형민_이모션 <hyeongmin.kim@emotion.co.kr>
2026-01-07 16:12:38 +0900

    이미지 프록시 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,11 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-      }
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
     }
   }
 })
